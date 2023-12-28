562d04979e5d58ea6f0eade7131453d22ecac696
winston <hey@winston.sh>
2023-12-28 04:33:42 +0100

    more insanity

--- flake.nix
+++ flake.nix
@@ -1,52 +1,66 @@
 {
-  inputs.mozilla-addons-to-nix.url = "sourcehut:~rycee/mozilla-addons-to-nix";
+  inputs = {
+    mozilla-addons-to-nix.url = "sourcehut:~rycee/mozilla-addons-to-nix";
+    addons = {
+      url = "path:./addons.json";
+      flake = false;
+    };
+  };
+
   outputs = {
     self,
     nixpkgs,
     mozilla-addons-to-nix,
+    addons,
   }: let
+    inherit (nixpkgs) lib;
     systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
-    eachSystem = fn: nixpkgs.lib.genAttrs systems (system: fn nixpkgs.legacyPackages.${system});
-    addons = ["simplediscordcrypt" "discord-container" "catppuccin-macchiato-lavender2"];
+    eachSystem = fn: lib.genAttrs systems (system: fn nixpkgs.legacyPackages.${system});
   in {
-    packages = eachSystem (pkgs: let
-      addon-list = pkgs.runCommand "build-addons" {
-        buildInputs = [mozilla-addons-to-nix.packages.${pkgs.system}.default];
-        addonJSON = builtins.toJSON (builtins.map (slug: {slug = slug;}) addons);
-        passAsFile = ["addonJSON"];
-      } "mozilla-addons-to-nix $addonListPath $out";
-      buildFirefoxXpiAddon = pkgs.lib.makeOverridable ({
-        stdenv ? pkgs.stdenv,
-        fetchurl ? pkgs.fetchurl,
-        pname,
-        version,
-        addonId,
-        url,
-        sha256,
-        meta,
-        ...
-      }:
-        stdenv.mkDerivation {
-          name = "${pname}-${version}";
+    packages = eachSystem (
+      pkgs: let
+        addonsJSON = builtins.readFile addons;
+        addonsList = lib.attrsets.catAttrs "slug" (builtins.fromJSON addonsJSON);
+        addonsDrv = pkgs.runCommand "build-addons" {
+          buildInputs = [mozilla-addons-to-nix.packages.${pkgs.system}.default];
+          passAsFile = ["addonsJSON"];
+        } "mozilla-addons-to-nix ${addons} $out";
+        addonListIFD = import addonsDrv {
+          inherit (pkgs) fetchurl lib stdenv;
+          inherit buildFirefoxXpiAddon;
+        };
+        buildFirefoxXpiAddon = lib.makeOverridable ({
+          stdenv ? pkgs.stdenv,
+          fetchurl ? pkgs.fetchurl,
+          pname,
+          version,
+          addonId,
+          url,
+          sha256,
+          meta,
+          ...
+        }:
+          stdenv.mkDerivation {
+            name = "${pname}-${version}";
 
-          inherit meta;
+            inherit meta;
 
-          src = fetchurl {inherit url sha256;};
+            src = fetchurl {inherit url sha256;};
 
-          preferLocalBuild = true;
-          allowSubstitutes = true;
+            preferLocalBuild = true;
+            allowSubstitutes = true;
 
-          buildCommand = ''
-            dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
-            mkdir -p "$dst"
-            install -v -m644 "$src" "$dst/${addonId}.xpi"
-          '';
-        });
-      addonListIFD = import addon-list {
-        inherit (pkgs) fetchurl lib stdenv;
-        inherit buildFirefoxXpiAddon;
-      };
-    in
-      pkgs.lib.attrsets.genAttrs addons (addon: addonListIFD.${addon}));
+            buildCommand = ''
+              dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
+              mkdir -p "$dst"
+              install -v -m644 "$src" "$dst/${addonId}.xpi"
+            '';
+          });
+      in
+        {
+          default = addonsDrv;
+        }
+        // lib.attrsets.genAttrs addonsList (addon: addonListIFD.${addon})
+    );
   };
 }
